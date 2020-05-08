9b1c54128165ea83b64ff27f1e3606c61eeeb573
Jordan <jordanjean17@gmail.com>
2020-05-08 01:38:51 -0700

    Initial Commit

---
+++ GOT_FinalProject_Maddox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GOT_FinalProject_Maddox
+//
+//  Created by Jordan Maddox on 5/8/20.
+//  Copyright © 2020 Jordan Maddox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
